fb6f60212fafaffa42672dabaa8eb44fa8f8a73d
Jay Wengrow <jaywngrw@gmail.com>
2014-11-18 14:48:50 -0600

    added destroy action and id in the json

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -7,5 +7,10 @@ class EmployeesController < ApplicationController
   def show
     @employee = Employee.find_by(id: params[:id]) 
   end
+
+  def destroy
+    @employee = Employee.find(params[:id])
+    @employee.destroy
+  end
   
 end
